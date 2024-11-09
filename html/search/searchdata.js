a3817a531e6d60df6d0f24cc8c1bff80e0118ebc
Luiz Eduardo <lzeduardo64@gmail.com>
2024-11-08 22:38:06 -0300

    add html folder for github pages

---
+++ html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "als",
+  1: "a",
+  2: "ls"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Arquivos",
+  2: "Funções"
+};
+
